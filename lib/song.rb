2dd54ec04b668c74dd1fc4a1f4608bb4fcd1652d
Greg Stimson <stimsong@yahoo.com>
2018-03-26 23:15:36 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,54 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@count +=1
+
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |gen|
+      if genre_count[gen]
+        genre_count[gen] += 1
+      else
+        genre_count[gen] = 1
+      end
+    end
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |art|
+      if artist_count[art]
+        artist_count[art] += 1
+      else
+        artist_count[art] = 1
+      end
+    end
+    artist_count
+  end
+
+end

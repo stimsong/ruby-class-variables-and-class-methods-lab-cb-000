8bcf7c5f941743ef5aebc3271dad5e2598554d73
Greg Stimson <stimsong@yahoo.com>
2018-03-26 22:46:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,7 @@ class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
   @@artists = []
+  @@genres = []
 
   def initialize(name, artist, genre)
     @name = name
@@ -11,6 +12,7 @@ class Song
     @@count +=1
 
     @@artists << artist
+    @@genres << genre
   end
 
   def self.count
